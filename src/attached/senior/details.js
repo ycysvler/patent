efe76ac0d05a38b503eefd27fa02ca2d8279bc16
VLER <ycysvler@hotmail.com>
2017-03-13 22:54:16 +0800

    + 妈的，好像要丢文件啊

---
+++ src/attached/senior/details.js
@@ -0,0 +1,134 @@
+/**
+ * Created by VLER on 2017/3/10.
+ */
+import React from 'react';
+import {Layout, Menu, Breadcrumb, Icon, Button} from 'antd';
+const {SubMenu} = Menu;
+const {Header, Content, Sider} = Layout;
+
+class AttachedSeniorDetails extends React.Component {
+    render() {
+        return (
+            <Layout >
+                <div className="breadcrumb">
+                    <Breadcrumb style={{margin: '11px 0'}}>
+                        <Breadcrumb.Item>高级检索</Breadcrumb.Item>
+                        <Breadcrumb.Item>历史查询</Breadcrumb.Item>
+                        <Breadcrumb.Item>查询结果</Breadcrumb.Item>
+                    </Breadcrumb>
+                </div>
+                <Layout className="content">
+                    <Content >
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                    </Content>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default AttachedSeniorDetails;
--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ import RoleList from './system/role/list.js';
 import ResourceList from './system/resource/list.js';
 import LogList from './system/log/list.js';
 
+import AttachedFastList from './attached/fast/list.js';
+import AttachedFastCreate from './attached/fast/create.js';
+import AttachedFastDetails from './attached/fast/details.js';
+
+import AttachedSeniorList from './attached/senior/list.js';
+import AttachedSeniorCreate from './attached/senior/create.js';
+import AttachedSeniorDetails from './attached/senior/details.js';
 
 import 'antd/dist/antd.css';
 import './styles/base.less';
@@ -32,10 +39,23 @@ const routes = (
             {/*日志查询*/}
             <Route path="/system/logs" component={LogList}/>
 
-            LogList
+            {/*附图.快速检索.历史查询*/}
+            <Route path="/attached/fast" component={AttachedFastList}/>
+            {/*新建快速查询*/}
+            <Route path="/attached/create" component={AttachedFastCreate}/>
+            {/*快速检索结果*/}
+            <Route path="/attached/details" component={AttachedFastDetails}/>
 
+            {/*附图.高级检索.历史查询*/}
+            <Route path="/senior/fast" component={AttachedSeniorList}/>
+            {/*新建快速查询*/}
+            <Route path="/senior/create" component={AttachedSeniorCreate}/>
+            {/*快速检索结果*/}
+            <Route path="/senior/details" component={AttachedSeniorDetails}/>
+
+            AttachedFastDetails
         </Route>
         {/*无此页面，转到登录*/}
-        <Route path="*" component={Login} />
+        <Route path="*" component={Login}/>
     </Router>);
 render(routes, document.getElementById('root'));
