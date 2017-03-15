da0b4c3897d80e728713fe3ce0ca18e98da0c58c
VLER <ycysvler@hotmail.com>
2017-03-16 01:43:29 +0800

    + 跑了一点点测试代码

--- src/welcome.js
+++ src/welcome.js
@@ -3,15 +3,44 @@
  */
 import React from 'react';
 import {Layout, Menu, Breadcrumb, Icon, Button} from 'antd';
+import Jquery from 'jquery';
+
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
 
 
+
 class Welcome extends React.Component {
+    onTest=function(){
+        console.log('aaaa');
+        var data = {
+            userid: "4dd3562851d641b09f78e074d672a221",
+            jobname: "aaasdaw",
+            typeids: [
+                "323234-23423",
+                "322222-23423"
+            ],
+            typenames: [
+                "nha",
+                "hb01"
+            ],
+            images: [
+                "/upload/admin/ses.jpg",
+                "/upload/admin/aaa.jpg"
+            ]
+        };
+
+
+        var promise = Jquery.post("http://114.247.108.199/attached/fast/create.ashx",JSON.stringify(data));
+        promise.done(function(data, status){
+            let o = JSON.parse(data);
+        });
+    }
     render() {
         return (
             <Layout >
                 Welcome
+                <Button onClick={this.onTest}>test</Button>
             </Layout>
         );
     }
