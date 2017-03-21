7f63a9a0bfa9a3a53a57bb652c4b9446bce2090e
VLER <ycysvler@hotmail.com>
2017-03-22 00:43:29 +0800

    在trigger里面增加了action参数，用于接收事件的地方判断是哪个action处罚的trigger

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React from 'react';
 import {Layout, Menu, Breadcrumb, Icon, Button} from 'antd';
 import Reflux from 'reflux';
 import {IndexActions, IndexStore} from './api.js';
-import { Link} from 'react-router';
+import {Link} from 'react-router';
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
 
 import './styles/app.css';
 
 const App = React.createClass({
-    mixins:[Reflux.listenTo(IndexStore,'onStatusChange')],
+    mixins: [Reflux.listenTo(IndexStore, 'onStatusChange')],
     getInitialState: function () {
-        return {"indexList": [],"leftIndex": []};
+        return {"indexList": [], "leftIndex": []};
     },
     componentDidMount: function () {
         IndexActions.getIndexes(1132);
     },
-    onStatusChange: function(data) {
-        console.log(data);
-        this.setState({indexList:data.data});
-        this.setState({leftIndex:data.data[0].children});
+    onStatusChange: function (action, data) {
+        // 判断一下action,当同一个Store多个不同的方法发出trigger时好区分是谁发的
+        if (acton === 'getIndexes') {
+            console.log(data);
+            this.setState({indexList: data.data});
+            this.setState({leftIndex: data.data[0].children});
+        }
     },
-    onClickHandler: function(e) {
-        for(let i=0;i<this.state.indexList.length;i++) {
-            if(this.state.indexList[i].rid == e.key) {
-                this.setState({leftIndex:this.state.indexList[i].children});
+    onClickHandler: function (e) {
+        for (let i = 0; i < this.state.indexList.length; i++) {
+            if (this.state.indexList[i].rid == e.key) {
+                this.setState({leftIndex: this.state.indexList[i].children});
                 return;
             }
         }
@@ -32,29 +35,30 @@ const App = React.createClass({
     render() {
         let lIndexs = this.state.leftIndex;
         let index_list = this.state.indexList;
-        console.log(lIndexs,index_list);
+        console.log(lIndexs, index_list);
         return (
             <Layout className="app-root">
                 <Header style={{background: '#3f3f3f'}}>
                     <div className="logo"/>
                     <div style={{float: 'right'}}>
-                        <Layout style={{"background":"#3f3f3f"}}>
+                        <Layout style={{"background": "#3f3f3f"}}>
                             <Content>
                                 <Menu
                                     theme="dark"
                                     mode="horizontal"
                                     style={{lineHeight: '64px'}}
                                     onClick={this.onClickHandler}
-                                    >
+                                >
                                     {
-                                        index_list.map(function(index) {
+                                        index_list.map(function (index) {
                                             return <Menu.Item key={index.rid}>{index.rname}</Menu.Item>
                                         })
                                     }
                                 </Menu></Content>
                             <Sider width={160} className="box"
                                    style={{background: '#3f3f3f', height: '64px', justifyContent: 'flex-end'}}>
-                                <Button style={{"background":"#3f3f3f","border":"none"}} icon="question-circle-o" size="small" className="header-help">帮助</Button>
+                                <Button style={{"background": "#3f3f3f", "border": "none"}} icon="question-circle-o"
+                                        size="small" className="header-help">帮助</Button>
                                 <div className="icon"></div>
                             </Sider>
                         </Layout>
@@ -64,13 +68,13 @@ const App = React.createClass({
                     <Sider width={250} style={{background: '#cccccc'}}>
                         <Menu
                             mode="inline"
-                            >
+                        >
                             {
-                                lIndexs.map(function(lIndex) {
-                                    if(lIndex.children.length > 0) {
+                                lIndexs.map(function (lIndex) {
+                                    if (lIndex.children.length > 0) {
                                         return <SubMenu key={lIndex.rid} title={<span>{lIndex.rname}</span>}>
                                             {
-                                                lIndex.children.map(function(i){
+                                                lIndex.children.map(function (i) {
                                                     return <Menu.Item key={i.rid}>{i.rname}</Menu.Item>
                                                 })
                                             }
