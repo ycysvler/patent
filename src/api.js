38a59e8ed52960388b48e30ed2bd6cd6d519949b
VLER <ycysvler@hotmail.com>
2017-03-22 01:36:54 +0800

    刚刚测试了一下https，将来正式商用时候，使用https

--- src/api.js
+++ src/api.js
@@ -12,7 +12,7 @@ const IndexStore = Reflux.createStore({
     listenables:[IndexActions],
 
     onGetIndexes: function(userid) {
-        let url = server_address + "/systems/menus.ashx?";
+        let url = window.server_address + "/systems/menus.ashx?";
         let param = {};
         let self = this;
         param.userid = userid;
