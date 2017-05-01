8220915172c510c481d43cb7f29ccc73f58663c7
VLER <ycysvler@hotmail.com>
2017-05-01 16:02:06 +0800

    * 去掉不使用组件的引用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, hashHistory, IndexRoute} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import App from './App';
 
 import Login from './login.js';
